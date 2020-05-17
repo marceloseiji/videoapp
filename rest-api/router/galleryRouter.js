6b6fba8896d6a8cbb1497a4a01fa99f4e579687b
marceloseiji <marcelo.kihon@gmail.com>
2020-05-17 14:45:18 -0300

    multer installed, galleryRouter multer diskStorage added

--- rest-api/router/galleryRouter.js
+++ rest-api/router/galleryRouter.js
@@ -6,6 +6,37 @@ const ResponseClass = require('../models/ResponseClass');
 
 let publicFolder = './public/files';
 
+let multer = require('multer');
+let path = require('path');
+
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, publicFolder)
+  },
+  filename: function (req, file, cb) {
+    //cb(null, file.fieldname + '-' + Date.now())
+    let fileName = `${file.fieldname.replace(/\//g, '')}-${Date.now()}${path.extname(file.originalname)}`;
+    req.body.path = publicFolder + fileName;
+  }
+})
+
+var upload = multer({storage: storage});
+
+router.post('/', function(req, resp, next) {
+  GalleryModel.getAll(function(error, returnData){
+    let response = new ResponseClass();
+
+    if(error) {
+      response.erro = true;
+      response.msg = "Oops some error here..." + error.message;
+      console.log("error: ", error.message);
+    } else {
+      response.data = returnData;
+    }
+    resp.json(response);
+  })
+});
+
 router.get('/', function(req, resp, next) {
   GalleryModel.getAll(function(error, returnData){
     let response = new ResponseClass();
