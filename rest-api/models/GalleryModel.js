f2eae553906af4206a76b48d508f7264dc1d8f66
marceloseiji <marcelo.kihon@gmail.com>
2020-05-17 13:56:02 -0300

    getId added

--- rest-api/models/GalleryModel.js
+++ rest-api/models/GalleryModel.js
@@ -1,7 +1,12 @@
 const db = require('../database/dbConnection.js');
 
 module.exports = class GalleryModel {
+
   static getAll(callback) {
     return db.query("select * from videos", callback);
+  }
+
+  static getId(id, callback) {
+    return db.query("select * from videos where video_id = ?", [id], callback);
   }
 }
