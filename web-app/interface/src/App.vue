8c035d359b41d6f8c0eb9d76f5311ccba1f6f178
marceloseiji <marcelo.kihon@gmail.com>
2020-05-19 20:12:18 -0300

    main components added, vue router installed

--- web-app/interface/src/App.vue
+++ web-app/interface/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Home></Home>
+    <Gallery></Gallery>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Gallery from './components/gallery/Gallery';
+import Home from './components/home/Home';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Gallery,
+    Home
   }
 }
 </script>
