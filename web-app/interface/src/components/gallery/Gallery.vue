a794b82d2b78123d29da97f77cedcbb754b6b33c
marceloseiji <marcelo.kihon@gmail.com>
2020-05-19 22:12:53 -0300

    mais vue files added

---
+++ web-app/interface/src/components/gallery/Gallery.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1></h1>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  props: {
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
